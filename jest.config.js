2918331632f6021d266fd9cb92792d362964dfee
mahmudmardini <M.Mardini152@gmail.com>
2021-08-18 07:56:33 +0300

    add jest configration

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,23 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
-}
+  moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
+  transform: {
+    "^.+\\.vue$": "vue-jest",
+    ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
+      "jest-transform-stub",
+    "^.+\\.(js|jsx)?$": "babel-jest"
+  },
+  preset: "@vue/cli-plugin-unit-jest",
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{js,vue}",
+    "!src/main.js",
+  ],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1"
+  },
+  snapshotSerializers: ["jest-serializer-vue"],
+  testMatch: [
+    "**/tests/unit/**/*.spec.{j,t}s?(x)"
+  ],
+  transformIgnorePatterns: ["<rootDir>/node_modules/"]
+};
